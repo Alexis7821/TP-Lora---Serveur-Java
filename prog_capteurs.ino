c5795b2ccedb5bb9c780137ebe570ac624322232
Alexis7821 <76158591+Alexis7821@users.noreply.github.com>
2021-11-03 16:51:52 +0100

    Create prog_capteurs.ino

---
+++ prog_capteurs.ino
@@ -0,0 +1,208 @@
+/*MOURLANETTE Alexis & BAHU Florian - I4GRIT - Réseaux IOT / Programme Arduino*/
+
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
+//Déclaration de la bibliothèque pour la température
+#include "TinyDHT.h"
+#define DHTPIN 4  // DHT connected to Arduino Uno Digital Pin 4
+#define DHTTYPE DHT11   // DHT 11 
+DHT dht(DHTPIN, DHTTYPE);
+
+// LoRaWAN NwkSKey, network session key
+// This is the default Semtech key, which is used by the prototype TTN
+// network initially.
+//ttn 
+static const PROGMEM u1_t NWKSKEY[16] = { 0xE7, 0xCD, 0x34, 0xD9, 0xDC, 0x18, 0xD3, 0x4E, 0x7E, 0xC8, 0xE5, 0xBA, 0x94, 0xD8, 0x6B, 0x0D };
+// LoRaWAN AppSKey, application session key
+// This is the default Semtech key, which is used by the prototype TTN
+// network initially.
+//ttn
+static const u1_t PROGMEM APPSKEY[16] = { 0xC1, 0x72, 0x30, 0x33, 0x7D, 0x35, 0x94, 0xB4, 0x22, 0xDE, 0x40, 0x41, 0x24, 0xE1, 0xE2, 0x09 };
+//
+// LoRaWAN end-device address (DevAddr)
+// See http://thethingsnetwork.org/wiki/AddressSpace
+// ttn
+static const u4_t DEVADDR = 0x260BD745;
+
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+static uint8_t mydata[] = {0x00, 0x00, 0x00}; //compteur, température, humidité
+static osjob_t initjob,sendjob,blinkjob;
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 10;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = 10,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = 9,
+    .dio = {2, 6, 7},
+};
+
+/*Lock la fréquence*/
+void SetChannels() {
+  LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+  LMIC_disableChannel(1);      // g-band
+  LMIC_disableChannel(2);      // g-band
+  LMIC_disableChannel(3);
+  LMIC_disableChannel(4);
+  LMIC_disableChannel(5);
+  LMIC_disableChannel(6);
+  LMIC_disableChannel(7);
+  LMIC_disableChannel(8);
+}
+
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println("OP_TXRXPEND, not sending");
+    } else {
+        mydata[2] = dht.readHumidity();
+        mydata[1] = dht.readTemperature();
+        mydata[0]++;
+        Serial.print(mydata[0]);
+        Serial.print(" | Temperature : ");
+        Serial.print(mydata[1]);
+        Serial.print(" °C");
+        Serial.print(" | Humidity : ");
+        Serial.print(mydata[2]);
+        Serial.print(" %\t");
+
+
+      
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, mydata, sizeof(mydata), 0);
+        Serial.println("Packet queued");
+        Serial.println(LMIC.freq);
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    Serial.println(ev);
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println("EV_SCAN_TIMEOUT");
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println("EV_BEACON_FOUND");
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println("EV_BEACON_MISSED");
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println("EV_BEACON_TRACKED");
+            break;
+        case EV_JOINING:
+            Serial.println("EV_JOINING");
+            break;
+        case EV_JOINED:
+            Serial.println("EV_JOINED");
+            break;
+        case EV_RFU1:
+            Serial.println("EV_RFU1");
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println("EV_JOIN_FAILED");
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println("EV_REJOIN_FAILED");
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println("EV_TXCOMPLETE (includes waiting for RX windows)");
+            if(LMIC.dataLen) {
+                // data received in rx slot after tx
+                Serial.print("Data Received: ");
+                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+                Serial.println();
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println("EV_LOST_TSYNC");
+            break;
+        case EV_RESET:
+            Serial.println("EV_RESET");
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println("EV_RXCOMPLETE");
+            break;
+        case EV_LINK_DEAD:
+            Serial.println("EV_LINK_DEAD");
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println("EV_LINK_ALIVE");
+            break;
+         default:
+            Serial.println("Unknown event");
+            break;
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    while(!Serial);
+
+    dht.begin();
+
+    Serial.println("Starting");
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+    //LMIC_setClockError(MAX_CLOCK_ERROR * 1/100);
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly 
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+    
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+    
+    // Set data rate and transmit power (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+    SetChannels();
+
+    // Start job
+    do_send(&sendjob);
+}
+
+void loop() {
+    os_runloop_once();
+}
